3c09b506aa5e81162215a819c9d30856ed6e2644
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-09 03:51:05 +0000

    About page updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+
 
 function About() {
   return (
     <div>
-        <h1>About Us</h1>
+      <h1>About Us</h1>
+      {/* <img src={gymImage} alt="Gym Workout" style={{ width: '100%', height: 'auto' }} /> */}
+      <p>Welcome to Fitness Fusion, where we turn sweat into success!</p>
+      <p>At Fitness Fusion, we're not just a gym; we're a community of fitness enthusiasts committed to transforming lives. Our team of certified personal trainers is here to guide you on your fitness journey.</p>
+      <p>What sets us apart? Our personalized workout tracking system! With state-of-the-art technology, we tailor your fitness plan to your unique goals. Track your progress, set new milestones, and celebrate your achievements with us.</p>
+      <p>Ready to crush your fitness goals? Join Fitness Fusion today and let's sculpt the new you!</p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
