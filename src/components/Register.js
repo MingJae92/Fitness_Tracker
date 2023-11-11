69118884beac02344705d653295b3b6e763df002
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-11 18:11:49 +0000

    Register form FE implemented

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import RegisterForm from '../img/bg.jpg';
 
 function Register() {
+  // Styles for the container, image, and header
   const containerStyle = {
     display: 'flex',
     flexDirection: 'column',
@@ -18,11 +19,13 @@ function Register() {
     padding: '20px',
   };
 
+  // State variables to store form input values
   const [userName, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  // Handle input changes in the form fields
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     switch (name) {
@@ -43,6 +46,7 @@ function Register() {
     }
   };
 
+  // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
     // Perform actions on form submission, e.g., send data to a server
@@ -50,11 +54,16 @@ function Register() {
     alert("You are now signed up!")
   };
 
+  // Render the component with the registration form
   return (
     <div style={containerStyle}>
+      {/* Display the registration form header */}
       <h1 style={headerStyle}>Register</h1>
+      {/* Display the background image */}
       <img src={RegisterForm} alt="Landing Page" style={imageStyle} />
+      {/* Define the registration form using React Bootstrap components */}
       <Form onSubmit={handleSubmit}>
+        {/* Input field for username */}
         <Form.Group controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
           <Form.Control
@@ -66,6 +75,7 @@ function Register() {
           />
         </Form.Group>
 
+        {/* Input field for email */}
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control
@@ -75,9 +85,11 @@ function Register() {
             value={email}
             onChange={handleInputChange}
           />
+          {/* Display a helper text for the email field */}
           <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
         </Form.Group>
 
+        {/* Input field for password */}
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password:</Form.Label>
           <Form.Control
@@ -89,6 +101,7 @@ function Register() {
           />
         </Form.Group>
 
+        {/* Input field to confirm password */}
         <Form.Group controlId="formBasicPasswordConfirm">
           <Form.Label>Confirm password:</Form.Label>
           <Form.Control
@@ -100,6 +113,7 @@ function Register() {
           />
         </Form.Group>
 
+        {/* Submit button to register */}
         <Button variant="primary" type="submit">
           Register
         </Button>
