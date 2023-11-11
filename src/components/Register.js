da7c6555f2423f3b60289492d5dd6d3533b356a1
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-11 18:05:12 +0000

    Sign up form implemented

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import RegisterForm from '../img/bg.jpg';
 
@@ -15,54 +15,95 @@ function Register() {
   };
 
   const headerStyle = {
-    padding: '20px', // Add equal padding to the header
+    padding: '20px',
   };
 
-  const footerStyle = {
-    background: '#333',
-    padding: '10px',
-    textAlign: 'center',
-    position: 'fixed',
-    bottom: 0,
-    width: '100%',
-    color: 'white',
+  const [userName, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'username':
+        setUsername(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      case 'confirmPassword':
+        setConfirmPassword(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  const linkStyle = {
-    color: 'white',
-    textDecoration: 'underline',
-    margin: '0 10px',
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform actions on form submission, e.g., send data to a server
+    console.log('Form submitted with data:', { userName, email, password, confirmPassword });
+    alert("You are now signed up!")
   };
 
   return (
     <div style={containerStyle}>
       <h1 style={headerStyle}>Register</h1>
       <img src={RegisterForm} alt="Landing Page" style={imageStyle} />
-      <Form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            name="username"
+            value={userName}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+          />
+          <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPasswordConfirm">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm Password" />
+          <Form.Label>Confirm password:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={handleInputChange}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Register
         </Button>
       </Form>
-
-      
     </div>
   );
 }
