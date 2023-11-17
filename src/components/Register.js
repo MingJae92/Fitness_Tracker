3fcdf2c3c71a0f24b8f052e3b09b19005a76fcc2
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-11-17 00:37:02 +0000

    Error messages added to registration form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Form, Button } from 'react-bootstrap'; // Import Form and Button from React Bootstrap
+import { Form, Button } from 'react-bootstrap';
 import RegisterForm from '../img/bg.jpg';
-
 
 function Register() {
   // Styles for the container, image, and header
@@ -24,16 +23,16 @@ function Register() {
   // State variables to store form input values
   const { register, handleSubmit, formState: { errors } } = useForm({
     defaultValues: {
-      username: 'name',
-      email: 'email',
-      password: 'password',
-      confirmPassword: 'confirm password', 
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     },
   });
 
   const handleRegister = (data) => {
     console.log(data);
-    alert("You are now registered!")
+    alert('You are now registered!');
   };
 
   return (
@@ -49,8 +48,13 @@ function Register() {
           <Form.Label>Username</Form.Label>
           <Form.Control
             type="text"
-            {...register('username', { required: 'Username required' })}
+            {...register('username', {
+              required: 'Username is required',
+              minLength: { value: 5, message: 'The username should have at least 5 characters' },
+              pattern: { value: /^[a-zA-Z0-9_]+$/, message: 'Username must contain only letters, numbers, and _' },
+            })}
           />
+          <div style={{ color: 'red' }}>{errors.username?.message}</div>
         </Form.Group>
 
         {/* Input field for email */}
@@ -58,8 +62,16 @@ function Register() {
           <Form.Label>Email</Form.Label>
           <Form.Control
             type="email"
-            {...register('email', { required: 'Email required' })}
+            {...register('email', {
+              required: 'Email required',
+              maxLength: { value: 50, message: 'The email should have at most 50 characters' },
+              pattern: {
+                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                message: 'Email address must be a valid address',
+              },
+            })}
           />
+          <div style={{ color: 'red' }}>{errors.email?.message}</div>
           {/* Display a helper text for the email field */}
           <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
         </Form.Group>
@@ -67,19 +79,15 @@ function Register() {
         {/* Input field for password */}
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            {...register('password', { required: 'Password required' })}
-          />
+          <Form.Control type="password" {...register('password', { required: 'Password required' })} />
+          <div style={{ color: 'red' }}>{errors.password?.message}</div>
         </Form.Group>
 
         {/* Input field to confirm password */}
         <Form.Group controlId="formBasicPasswordConfirm">
           <Form.Label>Confirm password:</Form.Label>
-          <Form.Control
-            type="password"
-            {...register('confirmPassword', { required: 'Password confirmation required' })}
-          />
+          <Form.Control type="password" {...register('confirmPassword', { required: 'Password confirmation required' })} />
+          <div style={{ color: 'red' }}>{errors.confirmPassword?.message}</div>
         </Form.Group>
 
         {/* Submit button to register */}
